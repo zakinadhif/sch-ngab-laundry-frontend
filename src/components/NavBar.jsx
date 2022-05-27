6209105175db771bf0f07f84069c3b44defb7fe2
cowdingus <zaki.nadhif45@gmail.com>
2022-05-28 06:03:43 +0700

    Write dark styles for login page and navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,47 @@
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
+  function toggleTheme() {
+    if (localStorage.theme === "light") {
+      localStorage.setItem("theme", "dark");
+    } else if (localStorage.theme === "dark") {
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
+  }
+
   return (
-    <nav className="fixed flex items-center justify-between w-full h-12 px-3 bg-white border-b">
-      <ul className="flex items-center gap-2">
-        <li><Link to="/" className="text-xl font-medium tracking-tight"><span className="font-bold text-purple-600">Ngab</span>Laundry</Link></li>
+    <nav className="fixed flex items-center justify-between w-full h-12 px-3 border-b dark:border-slate-700">
+      <ul className="flex items-center gap-3">
+        <li>
+          <Link to="/" className="text-xl font-medium tracking-tight">
+            <span className="font-bold text-purple-600">Ngab</span>Laundry
+          </Link>
+        </li>
+        <li>
+          <button
+            className="p-1 leading-none bg-gray-200 rounded-lg dark:bg-slate-600"
+            onClick={toggleTheme}
+          >
+            🌙
+          </button>
+        </li>
       </ul>
       <ul className="flex items-center gap-3">
-        <li><Link to="/login" className="text-md">Login</Link></li>
-        <li><Link to="/signup" className="px-2 py-1 text-lg text-white bg-purple-600 rounded-md">Sign Up</Link></li>
+        <li>
+          <Link to="/login" className="text-md">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/signup"
+            className="px-2 py-1 text-lg text-white bg-purple-600 rounded-md"
+          >
+            Sign Up
+          </Link>
+        </li>
       </ul>
     </nav>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,31 +2,31 @@ import NavBar from "../components/NavBar";
 
 export default function Login() {
   return (
-    <>
+    <div className="dark:text-white dark:bg-slate-900">
       <NavBar />
       <div className="flex min-h-screen pt-12">
         <div className="hidden md:flex justify-center items-center grow-[2]">
           <p>Welcome to Ngab Laundry</p>
         </div>
-        <div className="border-l flex flex-col items-center justify-center grow-[1]">
+        <div className="flex flex-col items-center justify-center grow-[1] border-l dark:border-slate-700">
           <form className="flex flex-col w-full max-w-xs gap-1">
             <h1 className="mb-1 text-2xl font-bold">Login</h1>
-            <label htmlFor="username-input" className="text-sm text-gray-800">
+            <label htmlFor="username-input" className="text-sm text-gray-500 dark:text-gray-400">
               Username
             </label>
             <input
               name="username"
               id="username-input"
-              className="p-1 rounded-md ring-slate-500 ring-1"
+              className="p-1 rounded-md ring-slate-500 ring-1 dark:bg-slate-500"
               type="text"
             />
-            <label htmlFor="password-input" className="text-sm text-gray-800">
+            <label htmlFor="password-input" className="text-sm text-gray-500 dark:text-gray-400">
               Password
             </label>
             <input
               name="password"
               id="password-input"
-              className="p-1 rounded-md ring-slate-500 ring-1"
+              className="p-1 rounded-md ring-slate-500 ring-1 dark:bg-slate-500"
               type="password"
             />
             <input
@@ -38,6 +38,6 @@ export default function Login() {
           </form>
         </div>
       </div>
-    </>
+    </div>
   );
 }
